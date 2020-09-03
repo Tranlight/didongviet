04edc214b9580bd9fca8b8c118f80675d2516b89
Tranlight <>
2020-09-04 00:01:34 +0700

    update config

--- system/config/config.php
+++ system/config/config.php
@@ -1,8 +1,8 @@
 <?php
 // Thông số database
-putenv("DB_HOSTNAME=localhost");
-putenv("DB_DATABASE=dienthoai");
+putenv("DB_HOSTNAME=db");
+putenv("DB_DATABASE=didongviet_dev");
 putenv("DB_USERNAME=root");
-putenv("DB_PASSWORD=");
+putenv("DB_PASSWORD=12345678");
 putenv("DB_PORT=3306");
 putenv("CHARSET=utf8");
